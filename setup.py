e1ecd60102e3e66422d21222fa113e1e48466a81
JClokey <joeclokey@gmail.com>
2023-01-11 15:16:36 +1000

    some updates
    Clean up for documentation

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='Passive',
       version='1.0',
+      license ='MIT',
       description='Passive sampling calculators',
       author='Joseph Clokey',
       author_email='joeclokey@gmail.com',
