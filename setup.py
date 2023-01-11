aea39e6daf7eb0a234559cb791d820672f133f0c
JClokey <joeclokey@gmail.com>
2023-01-11 15:20:23 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(name='Passive',
       description='Passive sampling calculators',
       author='Joseph Clokey',
       author_email='joeclokey@gmail.com',
-      packages=['water', 'learn'],
       zip_safe=False)
